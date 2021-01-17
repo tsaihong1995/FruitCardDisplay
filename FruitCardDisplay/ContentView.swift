53fb3979e0922e41b037ad9b7b2fb6473eeb5357
Hung-Chun, Tsai <tsaihong1995@gmail.com>
2021-01-17 14:36:41 -0500

    Initial Commit

---
+++ FruitCardDisplay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FruitCardDisplay
+//
+//  Created by Hung-Chun Tsai on 2021-01-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FruitCardDisplay/FruitCardDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitCardDisplayApp.swift
+//  FruitCardDisplay
+//
+//  Created by Hung-Chun Tsai on 2021-01-17.
+//
+
+import SwiftUI
+
+@main
+struct FruitCardDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
